cd4e75fe8b1de89c599ea27fc67d51fd73039d2d
Pritam Gajare <pritamgajare456@gmail.com>
2024-06-16 16:37:52 +0530

    about and project page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -2,6 +2,26 @@ import React from 'react'
 
 export default function About() {
   return (
-    <div>About</div>
-  )
+    <div className='min-h-screen flex items-center justify-center'>
+      <div className='max-w-2xl mx-auto p-3 text-center'>
+        <div>
+          <h1 className='text-3xl font font-semibold text-center my-7'>
+            About Prit' Blog
+          </h1>
+          <div className='text-md text-gray-500 flex flex-col gap-6'>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat adipisci excepturi quae, reiciendis inventore veritatis, molestias consequuntur dolorum, harum natus rerum quidem? Corporis fugiat id rerum distinctio ex, harum ut?
+            </p>
+
+            <p>
+Lorem ipsum dolor sit, amet consectetur adipisicing elit. Animi numquam hic repellat facere ullam. Voluptate reprehenderit ex unde sint veritatis, molestias repellat, maxime, dolore quos quia natus. Officia, debitis reiciendis.
+            </p>
+
+            <p>
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis voluptate asperiores officiis omnis id, aperiam sint consequatur eius enim. Neque doloribus quisquam consectetur id officiis distinctio fuga eum nostrum possimus.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
